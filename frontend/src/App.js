80112b2a5a51a1fb89517a2c2359648896ff42d7
roaris <one0803kyopro@gmail.com>
2021-09-22 16:42:36 +0900

    一覧取得を実装

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Post from './components/Post';
+
 const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_DEV_API_URL}/posts`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then(res => res.json())
+      .then(setPosts)
+  }, []);
+
   return (
-    <div></div>
+    <div>
+      <Box p={3}>
+        <Grid container spacing={4}>
+          {posts.map(post =>
+            <Grid item xs={4} key={post.id}>
+              <Post title={post.title} content={post.content} />
+            </Grid>
+          )}
+        </Grid>
+      </Box>
+    </div>
   );
 }
 
