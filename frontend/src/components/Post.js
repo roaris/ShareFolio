d2fdf8dc53c818c6190dcce947f4d73d8b3858da
roaris <one0803kyopro@gmail.com>
2021-09-22 16:31:26 +0900

    Postコンポーネントを作成

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,36 @@
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const Post = (props) => {
+  return (
+    <div>
+      <Card>
+        <CardContent>
+          <Typography style={{ fontSize: '25px' }}>{props.title}</Typography>
+          <Typography style={{ wordWrap: "break-word" }}>{props.content}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant='contained'>
+            Detail
+          </Button>
+          <Button
+            variant='contained'
+            color='primary'>
+            Edit
+          </Button>
+          <Button
+            variant='contained'
+            color='secondary'>
+            Delete
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
+};
+
+export default Post;
