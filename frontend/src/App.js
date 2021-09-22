1ac4969a8e3d6f0614f586902141a08c186fab66
roaris <one0803kyopro@gmail.com>
2021-09-23 02:18:45 +0900

    投稿機能を追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import CreateForm from './components/CreateForm';
 import Post from './components/Post';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
+  const [inputValue, setInputValue] = useState({ title: '', content: '' });
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_DEV_API_URL}/posts`, {
@@ -17,8 +19,43 @@ const App = () => {
       .then(setPosts)
   }, []);
 
+  const changeInputValue = (itemName, e) => {
+    const newInputValue = Object.assign({}, inputValue);
+    newInputValue[itemName] = e.target.value;
+    setInputValue(newInputValue)
+  }
+
+  const submitPost = () => {
+    fetch(`${process.env.REACT_APP_DEV_API_URL}/posts`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: inputValue.title,
+        content: inputValue.content,
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        const newPosts = posts.slice();
+        newPosts.push(res);
+        setPosts(newPosts);
+        setInputValue(
+          {
+            title: '',
+            content: '',
+          }
+        );
+      })
+  };
+
   return (
     <div>
+      <CreateForm
+        inputValue={inputValue}
+        changeInputValue={changeInputValue}
+        submitPost={submitPost} />
       <Box p={3}>
         <Grid container spacing={4}>
           {posts.map(post =>
