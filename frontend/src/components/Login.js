2a3c0756a73476e4daf3e51a6715bd36a5a63c16
roaris <one0803kyopro@gmail.com>
2021-10-02 19:05:13 +0900

    ログインページの作成

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const Login = () => {
+  const [inputValue, setInputValue] = useState({
+    email: '',
+    password: '',
+  });
+  const [errorMessage, setErrorMessage] = useState('');
+  const history = useHistory();
+
+  const changeInputValue = (itemName, e) => {
+    const newInputValue = Object.assign({}, inputValue);
+    newInputValue[itemName] = e.target.value;
+    setInputValue(newInputValue);
+  };
+
+  const login = () => {
+    fetch(`${process.env.REACT_APP_API_URL}/sessions`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ session: inputValue })
+    })
+      .then(res => {
+        if (res.status === 204) history.push('/home')
+        else if (res.status === 401) setErrorMessage('Invalid email/password combination');
+      })
+  };
+
+  const style = {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  };
+
+  return (
+    <Grid container alignItems='center' justifyContent='center' style={style}>
+      <Typography
+        style={{ color: 'red', marginBottom: 30 }}>
+        {errorMessage}
+      </Typography>
+      <TextField
+        label='メールアドレス'
+        variant='outlined'
+        style={{ width: '40ch', marginBottom: 30 }}
+        value={inputValue.email}
+        onChange={e => changeInputValue('email', e)} />
+      <TextField
+        label='パスワード'
+        type='password'
+        variant='outlined'
+        style={{ width: '40ch', marginBottom: 30 }}
+        value={inputValue.password}
+        onChange={e => changeInputValue('password', e)} />
+      <Button
+        variant='contained'
+        color='primary'
+        onClick={login}>
+        ログイン
+      </Button>
+    </Grid>
+  );
+};
+
+export default Login;
