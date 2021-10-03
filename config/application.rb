24599547cd7ad90eb085b956316cb990920febbb
roaris <one0803kyopro@gmail.com>
2021-10-03 21:34:45 +0900

    cookieを保存できるように修正

--- config/application.rb
+++ config/application.rb
@@ -35,14 +35,15 @@ module ReactRails
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins ENV["FRONTEND_ORIGIN"]
         resource "*",
           headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
       end
     end
   end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+if Rails.env === 'production'
+  Rails.application.config.session_store :cookie_store, key: '_react_rails', domain: ENV["FRONTEND_ORIGIN"]
+else
+  Rails.application.config.session_store :cookie_store, key: '_react_rails'
+end
