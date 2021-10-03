03afaf1d0bedbbf3ff5b17954813455fd1555f09
roaris <one0803kyopro@gmail.com>
2021-10-03 22:00:26 +0900

    アクセス制御を実装

---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(null);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/logged_in`, {
+      method: 'GET',
+      credentials: 'include'
+    })
+      .then(res => res.json())
+      .then(res => {
+        setLoggedIn(res.logged_in)
+      })
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  )
+};
+
+export default AuthContextProvider;
