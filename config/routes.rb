7eea6a64de783681d82ebdd0f7571e587f16ad44
roaris <one0803kyopro@gmail.com>
2021-10-05 23:11:56 +0900

    herokuでもreact-routerが動くように修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
       resources :posts
     end
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
