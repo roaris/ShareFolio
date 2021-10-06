a2327ac4958145ced802532a2ccd5fe66f455268
roaris <one0803kyopro@gmail.com>
2021-10-07 05:37:26 +0900

    fix typo

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    sequence(:name) { |n| "test"}
-    sequence(:email) { |n| "test@example.com"}
-    sequence(:password) { |n| "password"}
+    sequence(:name) {"test"}
+    sequence(:email) {"test@example.com"}
+    sequence(:password) {"password"}
   end
 end
