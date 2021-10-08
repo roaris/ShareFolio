8ade29566e0d3df6e61544b767f9f6390b2ca673
roaris <one0803kyopro@gmail.com>
2021-10-09 06:04:05 +0900

    Postモデルのテストを作成

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
-Rspec.describe Post, type: :model do
-  
+RSpec.describe Post, type: :model do
+  before do
+    FactoryBot.create(:user)
+  end
+
+  it 'is valid' do
+    post = FactoryBot.build(:post)
+    expect(post).to be_valid
+  end
+
+  it 'is invalid with an empty title' do
+    post = FactoryBot.build(:post, title: '')
+    expect(post).to be_invalid
+  end
+
+  it 'is invalid with a too long title' do
+    post = FactoryBot.build(:post, title: 'a'*31)
+    expect(post).to be_invalid
+  end
+
+  it 'is invalid with an empty content' do
+    post = FactoryBot.build(:post, content: '')
+    expect(post).to be_invalid
+  end
+
+  it 'is invalid with a too long content' do
+    post = FactoryBot.build(:post, content: 'a'*141)
+    expect(post).to be_invalid
+  end
+
+  it 'is deleted when an owner is deleted' do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post, user_id: user.id)
+    expect do
+      user.destroy
+    end.to change(Post, :count).by(-1)
+  end
 end
