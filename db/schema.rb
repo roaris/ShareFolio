f69af4a5798a06139dfe12ae75d9a3d76bde1d9c
roaris <one0803kyopro@gmail.com>
2021-10-09 04:23:36 +0900

    postsテーブルにuser_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_122511) do
+ActiveRecord::Schema.define(version: 2021_10_08_191141) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_09_28_122511) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
