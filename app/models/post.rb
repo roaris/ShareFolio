f8502e6ac0afffa654febee92715f7a824f74d78
roaris <one0803kyopro@gmail.com>
2021-10-09 05:35:20 +0900

    Postモデルのバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
-  validates :content, presence: true
+  validates :title, presence: true, length: {maximum: 30}
+  validates :content, presence: true, length: {maximum: 140}
   validates :user_id, presence: true
 end
---
+++ spec/models/posts_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+Rspec.describe Post, type: :model do
+  
+end
