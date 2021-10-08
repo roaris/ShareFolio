65f50ba9d218d1c7eb1d864e7cc0d9f7a9120101
roaris <one0803kyopro@gmail.com>
2021-10-09 04:43:56 +0900

    PostモデルとUserモデルのアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
   validates :name, {presence: true, length: {minimum: 3, maximum: 20}}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, {
