212d5f07677672e3e50e41ae3715deaaaddad205
roaris <one0803kyopro@gmail.com>
2021-10-20 16:35:51 +0900

    /api/v1/sessionsのテスト修正, 追加

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe '/api/v1/sessions', type: :request do
     it 'accept a valid user' do
       session_params = { session: { email: user.email, password: user.password } }
       post '/api/v1/sessions', params: session_params.to_json, headers: xhr_header.merge(json_header)
-      expect(response.status).to eq(204)
+      expect(response.status).to eq(200)
+      expect(json(response)['user_name']).to eq(user.name)
     end
 
     it 'reject an invalid user' do
@@ -30,6 +31,7 @@ RSpec.describe '/api/v1/sessions', type: :request do
       log_in_as(user)
       get '/api/v1/sessions/logged_in', headers: xhr_header
       expect(json(response)['logged_in']).to eq(true)
+      expect(json(response)['user_name']).to eq(user.name)
     end
 
     it 'return false for an unauth user' do
@@ -39,6 +41,25 @@ RSpec.describe '/api/v1/sessions', type: :request do
 
     it 'protect from CSRF' do
       get '/api/v1/sessions/logged_in'
+      expect(response.status).to eq(403)
+    end
+  end
+
+  describe 'logout' do
+    it 'accept an auth user' do
+      log_in_as(user)
+      delete '/api/v1/sessions/logout', headers: xhr_header
+      expect(response.status).to eq(204)
+    end
+
+    it 'reject an unauth user' do
+      delete '/api/v1/sessions/logout', headers: xhr_header
+      expect(response.status).to eq(400)
+    end
+
+    it 'protect from CSRF' do
+      log_in_as(user)
+      delete '/api/v1/sessions/logout'
       expect(response.status).to eq(403)
     end
   end
