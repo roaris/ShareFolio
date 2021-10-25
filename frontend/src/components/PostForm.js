b84e57d66fe3ca2d9b22fd65a91b9061e8527f65
roaris <one0803kyopro@gmail.com>
2021-10-25 23:39:50 +0900

    投稿保存のエラーハンドリングを追加

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -17,6 +17,12 @@ const PostForm = () => {
     app_url: '',
     repo_url: '',
   });
+  const [validationMessage, setValidationMessage] = useState({
+    app_name: '',
+    app_url: '',
+    repo_url: '',
+    description: '',
+  });
   const [markdown, setMarkdown] = useState();
 
   const changeInputValue = (itemName, e) => {
@@ -36,6 +42,21 @@ const PostForm = () => {
       body: JSON.stringify({
         post: Object.assign({}, inputValue, { description: markdown }),
       }),
+    }).then((res) => {
+      if (res.status === 400) {
+        res.json().then((err) => {
+          const newValidationMessage = {
+            app_name: '',
+            app_url: '',
+            repo_url: '',
+            description: '',
+          };
+          for (const property in err) {
+            newValidationMessage[property] = err[property][0];
+          }
+          setValidationMessage(newValidationMessage);
+        });
+      }
     });
   };
 
@@ -47,6 +68,8 @@ const PostForm = () => {
         style={{ width: 400, marginBottom: 30 }}
         value={inputValue.appName}
         onChange={(e) => changeInputValue('app_name', e)}
+        error={validationMessage.app_name !== ''}
+        helperText={validationMessage.app_name}
       />
       2. デプロイ先のURLを入力してください。
       <TextField
@@ -54,6 +77,8 @@ const PostForm = () => {
         style={{ width: 400, marginBottom: 30 }}
         value={inputValue.appUrl}
         onChange={(e) => changeInputValue('app_url', e)}
+        error={validationMessage.app_url !== ''}
+        helperText={validationMessage.app_url}
       />
       3. レポジトリのURLを入力してください。
       <TextField
@@ -61,9 +86,14 @@ const PostForm = () => {
         style={{ width: 400, marginBottom: 30 }}
         value={inputValue.repoUrl}
         onChange={(e) => changeInputValue('repo_url', e)}
+        error={validationMessage.repo_url}
+        helperText={validationMessage.repo_url}
       />
       4. アプリの概要や工夫点を入力してください。
       <SimpleMDE value={markdown} onChange={(e) => setMarkdown(e)} />
+      <p style={{ color: 'red', padding: 5 }}>
+        {validationMessage.description}
+      </p>
       <Button
         variant='contained'
         color='primary'
