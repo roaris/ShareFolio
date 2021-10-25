642180cbd33ab54719022f396645a5b67207aa76
roaris <one0803kyopro@gmail.com>
2021-10-25 23:26:00 +0900

    投稿の保存リクエストを実装

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -12,19 +12,64 @@ const PostForm = () => {
     paddingTop: 100,
   };
 
+  const [inputValue, setInputValue] = useState({
+    app_name: '',
+    app_url: '',
+    repo_url: '',
+  });
   const [markdown, setMarkdown] = useState();
+
+  const changeInputValue = (itemName, e) => {
+    const newInputValue = Object.assign({}, inputValue);
+    newInputValue[itemName] = e.target.value;
+    setInputValue(newInputValue);
+  };
+
+  const submitPost = () => {
+    fetch(`${process.env.REACT_APP_API_URL}/posts`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      body: JSON.stringify({
+        post: Object.assign({}, inputValue, { description: markdown }),
+      }),
+    });
+  };
 
   return (
     <Grid container alignItems='center' justifyContent='center' style={style}>
       1. アプリ名を入力してください。
-      <TextField variant='outlined' style={{ width: 400, marginBottom: 30 }} />
+      <TextField
+        variant='outlined'
+        style={{ width: 400, marginBottom: 30 }}
+        value={inputValue.appName}
+        onChange={(e) => changeInputValue('app_name', e)}
+      />
       2. デプロイ先のURLを入力してください。
-      <TextField variant='outlined' style={{ width: 400, marginBottom: 30 }} />
+      <TextField
+        variant='outlined'
+        style={{ width: 400, marginBottom: 30 }}
+        value={inputValue.appUrl}
+        onChange={(e) => changeInputValue('app_url', e)}
+      />
       3. レポジトリのURLを入力してください。
-      <TextField variant='outlined' style={{ width: 400, marginBottom: 30 }} />
+      <TextField
+        variant='outlined'
+        style={{ width: 400, marginBottom: 30 }}
+        value={inputValue.repoUrl}
+        onChange={(e) => changeInputValue('repo_url', e)}
+      />
       4. アプリの概要や工夫点を入力してください。
       <SimpleMDE value={markdown} onChange={(e) => setMarkdown(e)} />
-      <Button variant='contained' color='primary' style={{ marginBottom: 30 }}>
+      <Button
+        variant='contained'
+        color='primary'
+        style={{ marginBottom: 30 }}
+        onClick={submitPost}
+      >
         送信
       </Button>
     </Grid>
