f027ccef768787ac49a9718154e6b4bad59693e4
roaris <one0803kyopro@gmail.com>
2021-10-25 15:30:32 +0900

    postsテーブルのカラムを変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_044407) do
+ActiveRecord::Schema.define(version: 2021_10_25_060522) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "app_name", null: false
+    t.string "app_url", null: false
+    t.string "repo_url"
+    t.text "description", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
