9af956c5e3744b403308dbcce7c62dd2ae402833
roaris <one0803kyopro@gmail.com>
2021-10-29 19:02:02 +0900

    一覧表示ページのテストのために説明文を長くする

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     sequence(:app_name) { |n| "app#{n}" }
     sequence(:app_url) { |n| "https://example.com/app#{n}" }
     sequence(:repo_url) { |n| "https://example.com/repo#{n}" }
-    sequence(:description) { |n| "description#{n}" }
+    sequence(:description) { |n| "description#{n}" * 100 }
     sequence(:user_id) { 1 }
   end
 end
