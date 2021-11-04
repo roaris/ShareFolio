d9d72479f9b6ebf89655112d5f0c20e35863ada0
roaris <one0803kyopro@gmail.com>
2021-11-04 22:28:36 +0900

    デフォルトアイコンの表示

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -4,6 +4,7 @@ import DOMPurify from 'dompurify';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import logo from '../logo.svg';
 
 const PostDetail = (props) => {
   const { params } = props.match;
@@ -90,7 +91,7 @@ const PostDetail = (props) => {
       <Grid item xs={10} sm={10} md={6} lg={6}>
         <Grid container>
           <Grid item xs={12} lg={1} className={classes.postDetailLeft}>
-            <img src={userIconUrl} className={classes.icon} />
+            <img src={userIconUrl ? userIconUrl : logo} className={classes.icon} />
             <span className={classes.userName}>{userName}</span>
           </Grid>
           <Grid item xs={12} lg={11} className={classes.postDetailRight}>
