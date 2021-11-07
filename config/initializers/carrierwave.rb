04521f1426015c609c95b9f8a0be38d2c85a8fc3
roaris <one0803kyopro@gmail.com>
2021-11-07 19:46:20 +0900

    画像をS3に保存する設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
-CarrierWave.configure do |config|
-  config.asset_host = ENV['BACKEND_ORIGIN']
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :region => ENV['S3_REGION'],
+      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+    }
+    config.fog_directionary = ENV['S3_BUCKET']
+  end
+else
+  CarrierWave.configure do |config|
+    config.asset_host = ENV['BACKEND_ORIGIN']
+  end
 end
