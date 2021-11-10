16ce3589d55ef8d728872824200de2096dd8b341
roaris <one0803kyopro@gmail.com>
2021-11-10 18:20:08 +0900

    直近4件の投稿を表示

--- frontend/src/components/Top.js
+++ frontend/src/components/Top.js
@@ -1,12 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import Post from './Post';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
 const Top = () => {
+  const [postsAndUsers, setPostsAndUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/posts/recent`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    })
+      .then(res => res.json())
+      .then(setPostsAndUsers)
+  }, []);
+
   const styles = makeStyles({
     top: {
-      alignItems: 'center',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
@@ -28,6 +43,20 @@ const Top = () => {
           <h1>ShareFolio</h1>
           <p>ShareFolioはWebエンジニアを目指す人のためのポートフォリオプラットフォームです。</p>
         </Grid>
+      </Grid>
+      <h1 style={{marginLeft: 20}}>最近の投稿</h1>
+      <Grid container>
+        {postsAndUsers.map((postAndUser) => (
+          <Grid item xs={12} sm={12} md={6} key={postAndUser.post.id}>
+            <Grid container>
+              <Grid item xs={1} />
+              <Grid item xs={10} style={{ marginBottom: 20 }}>
+                <Post postAndUser={postAndUser} />
+              </Grid>
+              <Grid item xs={1} />
+            </Grid>
+          </Grid>
+        ))}
       </Grid>
       <div style={{ display: 'flex' }}>
         <Button
