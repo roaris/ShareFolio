eb694792ba42451339f70c3f03b7e753242a6d08
roaris <one0803kyopro@gmail.com>
2021-11-10 17:12:51 +0900

    ジャンボトロンを設置

--- frontend/src/components/Top.js
+++ frontend/src/components/Top.js
@@ -1,25 +1,34 @@
-import logo from '../logo.svg';
-import './styles/logo.css';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
 const Top = () => {
-  const style = {
-    textAlign: 'center',
-    backgroundColor: '#282c34',
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 'calc(15px + 2vmin)',
-    color: 'white',
-  };
+  const styles = makeStyles({
+    top: {
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      paddingTop: 50,
+    },
+    jumbotron: {
+      backgroundColor: 'lightgray',
+      fontSize: 'calc(1px + 3vmin)',
+      padding: 30,
+    }
+  });
+
+  const classes = styles();
 
   return (
-    <div style={style}>
-      <img src={logo} className='App-logo' alt='logo' />
-      <p>react_rails</p>
+    <div className={classes.top}>
+      <Grid container>
+        <Grid item xs={12} className={classes.jumbotron}>
+          <h1>ShareFolio</h1>
+          <p>ShareFolioはWebエンジニアを目指す人のためのポートフォリオプラットフォームです。</p>
+        </Grid>
+      </Grid>
       <div style={{ display: 'flex' }}>
         <Button
           to='/signup'
