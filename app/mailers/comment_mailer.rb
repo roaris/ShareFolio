0c172df6c9a44eb3ae24c9bdc9ad423bcb1d4495
roaris <one0803kyopro@gmail.com>
2021-11-14 14:43:32 +0900

    メーラーとメールビューを作成

---
+++ app/mailers/comment_mailer.rb
@@ -0,0 +1,13 @@
+class CommentMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.comment_mailer.comment_notification.subject
+  #
+  def comment_notification(comment)
+    @post = comment.post
+    @user = @post.user
+    mail to: @user.email, subject: "[ShareFolioからのお知らせ] コメントがつきました！"
+  end
+end
