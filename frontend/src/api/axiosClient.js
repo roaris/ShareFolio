9af0e4199a3df2ba98bacd5f383e753c3a47f6cb
roaris <one0803kyopro@gmail.com>
2021-11-25 23:49:34 +0900

    withCredentials: trueを削除

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -10,25 +10,21 @@ export const axiosClient = {
   get: (path) => {
     return axios.get(`${process.env.REACT_APP_API_URL}${path}`, {
       headers: headers,
-      withCredentials: true,
     });
   },
   post: (path, data) => {
     return axios.post(`${process.env.REACT_APP_API_URL}${path}`, data, {
       headers: headers,
-      withCredentials: true,
     });
   },
   patch: (path, data) => {
     return axios.patch(`${process.env.REACT_APP_API_URL}${path}`, data, {
       headers: headers,
-      withCredentials: true,
     });
   },
   delete: (path) => {
     return axios.delete(`${process.env.REACT_APP_API_URL}${path}`, {
       headers: headers,
-      withCredentials: true,
     })
   }
 };
@@ -42,25 +38,21 @@ export const axiosAuthClient = {
   get: async (path) => {
     return axios.get(`${process.env.REACT_APP_API_URL}${path}`, {
       headers: Object.assign({}, headers, await AuthorizationHeader()),
-      withCredentials: true,
     });
   },
   post: async (path, data) => {
     return axios.post(`${process.env.REACT_APP_API_URL}${path}`, data, {
       headers: Object.assign({}, headers, await AuthorizationHeader()),
-      withCredentials: true,
     });
   },
   patch: async (path, data) => {
     return axios.patch(`${process.env.REACT_APP_API_URL}${path}`, data, {
       headers: Object.assign({}, headers, await AuthorizationHeader()),
-      withCredentials: true,
     });
   },
   delete: async (path) => {
     return axios.delete(`${process.env.REACT_APP_API_URL}${path}`, {
       headers: Object.assign({}, headers, await AuthorizationHeader()),
-      withCredentials: true,
     })
   }
 };
