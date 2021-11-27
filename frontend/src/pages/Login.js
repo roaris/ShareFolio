db391aeec46e2cb3f305c75b3bc1296286f82de2
roaris <one0803kyopro@gmail.com>
2021-11-28 02:25:01 +0900

    Twitterアカウント認証を追加した

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,12 +2,13 @@ import React, { useState, useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { FlashMessageContext } from '../contexts/FlashMessageContext';
 import { axiosAuthClient } from '../api/axiosClient';
-import { auth, githubProvider, googleProvider } from '../firebase';
+import { auth, githubProvider, googleProvider, twitterProvider } from '../firebase';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import CheckIcon from '@mui/icons-material/Check';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import GoogleIcon from '@mui/icons-material/Google';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Login = () => {
   const setLoggedIn = useContext(AuthContext).setLoggedIn;
@@ -66,6 +67,29 @@ const Login = () => {
       });
   };
 
+  const twitterlogin = () => {
+    auth
+      .signInWithPopup(twitterProvider)
+      .then((res) => {
+        const name = res.user.displayName;
+        const email = res.user.email;
+        const icon = res.user.photoURL;
+        const token = res.user.Aa;
+        if (email === null) {
+          setErrorMessage('このTwitterアカウントにはメールアドレスが登録されていません。アドレス登録後に再度お手続きをお願いします。')
+          return;
+        }
+        createUser(name, email, icon, token);
+      })
+      .catch((err) => {
+        if (err.code === 'auth/account-exists-with-different-credential') {
+          setErrorMessage(
+            '既にこのメールアドレスは登録されています。違うログイン方法を選択してください。'
+          );
+        }
+      });
+  };
+
   const style = {
     paddingTop: 200,
   };
@@ -104,12 +128,24 @@ const Login = () => {
         <Button
           style={{
             border: 'solid 1px #bbb',
+            marginBottom: 20,
             textTransform: 'none',
           }}
           onClick={googlelogin}
         >
           <GoogleIcon style={{ color: '#DC4A3D', marginRight: 10 }} />
           Googleでログイン / 新規登録
+        </Button>
+        <Button
+          style={{
+            backgroundColor: '#4099FF',
+            color: 'white',
+            textTransform: 'none',
+          }}
+          onClick={twitterlogin}
+        >
+          <TwitterIcon style={{ marginRight: 10 }} />
+          Twitterでログイン / 新規登録
         </Button>
       </div>
     </Grid>
