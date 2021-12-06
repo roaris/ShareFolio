917dd8f2f99d50a4b7eb354b505f062eab143fef
roaris <one0803kyopro@gmail.com>
2021-12-06 23:19:40 +0900

    /users/meをresourcesの中に移動

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get '/users/me', to: 'users#show_me'
-      patch '/users/me', to: 'users#update_me'
       resources :users, only: %i[show create] do
         collection do
           get :search
+          get '/me', to: 'users#show_me'
+          patch '/me', to: 'users#update_me'
         end
         member do
           get :posts
