a2e0d7c494f3855b27c7d5e6131bbad4149873e2
roaris <one0803kyopro@gmail.com>
2021-12-19 21:28:35 +0900

    tagsテーブルのnameに一意性制約を付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_17_070024) do
+ActiveRecord::Schema.define(version: 2021_12_19_122336) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_12_17_070024) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
