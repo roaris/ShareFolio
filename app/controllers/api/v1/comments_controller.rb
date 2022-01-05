942f96471e2753185b543d19e6678468ebdcf069
roaris <one0803kyopro@gmail.com>
2022-01-05 20:55:16 +0900

    PATCH /posts/:post_id/comments/id を実装

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,13 +3,22 @@
 module Api
   module V1
     class CommentsController < ApplicationController
-      before_action :ensure_correct_user, only: %i[destroy]
+      before_action :ensure_correct_user, only: %i[update, destroy]
 
       def create
         comment = current_user.comments.build(comment_params.merge({ post_id: params[:post_id] }))
         if comment.save
           CommentMailer.comment_notification(comment).deliver_now if current_user.id != comment.post.user.id
           render status: :created, json: comment
+        else
+          render status: :bad_request, json: comment.errors
+        end
+      end
+
+      def update
+        comment = Comment.find(params[:id])
+        if comment.update(comment_params)
+          render status: :ok, json: comment
         else
           render status: :bad_request, json: comment.errors
         end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
         collection do
           get :recent
         end
-        resources :comments, only: %i[create destroy]
+        resources :comments, only: %i[create update destroy]
         resources :likes, only: [:create] do
           collection do
             delete '', to: 'likes#destroy'
