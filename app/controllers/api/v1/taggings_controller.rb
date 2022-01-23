6c851a0294534f26883eef01064f649609df4298
roaris <one0803kyopro@gmail.com>
2022-01-23 22:45:47 +0900

    TaggingsControllerのアクセス制御+テスト追加

--- app/controllers/api/v1/taggings_controller.rb
+++ app/controllers/api/v1/taggings_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module V1
     class TaggingsController < ApplicationController
+      before_action :ensure_correct_user
+
       def update
         post_id = params[:post_id]
         Tagging.where(post_id: post_id).delete_all
@@ -10,6 +12,12 @@ module Api
           { post_id: post_id, tag_id: tag_id, created_at: Time.zone.now, updated_at: Time.zone.now }
         end)
         render status: :ok
+      end
+
+      private
+
+      def ensure_correct_user
+        render status: :forbidden unless current_user.posts.find_by(id: params[:post_id])
       end
     end
   end
